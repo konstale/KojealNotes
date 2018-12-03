7a521236244ae99517d103ddc666bcd76db48d24
Jere Leppanen <jereale@metropolia.fi>
2018-12-03 15:10:10 +0200

    commented out error

--- app/src/main/java/com/example/notetest/MainActivity.java
+++ app/src/main/java/com/example/notetest/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lv = findViewById(R.id.deckView);
 
-        lv.setAdapter(new ArrayAdapter<Deck>(this, android.R.layout.simple_list_item_1, Decks.getInstance().getDecks()));
+        //lv.setAdapter(new ArrayAdapter<Deck>(this, android.R.layout.simple_list_item_1, Decks.getInstance().getDecks()));
 
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
