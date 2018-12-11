6a134030e923f19f737e84d54e6eee2e56a5e395
Jere Leppanen <jereale@metropolia.fi>
2018-12-11 10:37:22 +0200

    removed save button create activity lines

--- app/src/main/java/com/example/notetest/CreateDeckActivity.java
+++ app/src/main/java/com/example/notetest/CreateDeckActivity.java
@@ -4,14 +4,38 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class CreateDeckActivity extends AppCompatActivity {
+
+    Button saveButton;
+    EditText txt;
+    ListView show;
+Bundle extras = getIntent().getExtras();
+int i = extras.getInt("EXTRA");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_deck);
 
+            txt = (EditText) findViewById(R.id.changeText);
+            saveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String getInput = txt.getText().toString();
+                    Decks.getInstance().getDecks(i);
+
+
+
+                }
+
+            });
 
     }
 
